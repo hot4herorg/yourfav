445c3ada3c88ae462ad2c7b0544c2249e100798f
Hot4Her <hot4her@yourfav.com>
2015-07-26 15:55:16 -0400

    conditional toggle fav button

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,7 +1,8 @@
 module VideosHelper
 
 	def toggle_favorite_link(video, text=true)
-		link_to fa_icon('heart', text: text ? 'Favorite' : nil), toggle_favorite_video_path(video), class: "toggleFavVideo#{video.id} btn btn-sm btn-#{video.is_favorited?(current_user, true)}", remote: true
+		link_path = user_signed_in? ? toggle_favorite_video_path(video) : new_user_session_path
+		link_to fa_icon('heart', text: text ? 'Favorite' : nil), link_path, class: "toggleFavVideo#{video.id} btn btn-sm btn-#{video.is_favorited?(current_user, true)}", remote: user_signed_in?
 	end
 
 end
