ba7e6458cbbf6507af3acf74ed28520f00d3c36e
Hot4Her <hot4her@yourfav.com>
2016-05-22 13:44:38 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425194205) do
+ActiveRecord::Schema.define(version: 20160522173203) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20160425194205) do
 
   add_index "gallery_videos", ["gallery_id"], name: "index_gallery_videos_on_gallery_id"
   add_index "gallery_videos", ["video_id"], name: "index_gallery_videos_on_video_id"
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sites", force: :cascade do |t|
     t.string   "name"
