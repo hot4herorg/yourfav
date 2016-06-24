6f175096f7ea7b9907d416397df9c599bfb3d124
Hot4Her <hot4her@yourfav.com>
2016-06-23 20:04:25 -0400

    helper; link stars in a string

--- app/helpers/stars_helper.rb
+++ app/helpers/stars_helper.rb
@@ -10,4 +10,39 @@ module StarsHelper
 		end
 	end
 
+	def link_stars_in_str(stars, str)
+
+		stars_progression = []
+		stars.each do |star|
+			link = link_to star.name, star, title: star.name
+			str = str.gsub(star.name, link)
+			unless str.to_s.include? link.to_s
+				stars_progression << star
+			end
+		end if stars.any?
+
+		uncredited = []
+		stars_progression.each do |star|
+			first_name = star.name.split(" ").first
+			link = link_to first_name, star, title: star.name
+			str = str.gsub(first_name, link)
+			unless str.to_s.include? link.to_s
+				uncredited << star
+			end
+		end if stars_progression.any?
+
+		uncredited_str = []
+		uncredited.each do |star|
+			first_name = star.name.split(" ").first
+			link = link_to first_name, star, title: star.name, class: 'small'
+			uncredited_str << link
+		end if uncredited.any?
+
+		str = str + ' ' + uncredited_str.join('<small>, </small>') if uncredited_str.any?
+
+		str.html_safe
+	rescue
+		str
+	end
+
 end
